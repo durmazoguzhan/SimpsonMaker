ad1b8b3a5f17603a47be0209364ea3a06faa30cc
durmazoguzhan <durmazoguzhan@yahoo.com>
2022-11-08 02:15:06 +0300

    last commit

--- app/src/main/java/com/oguzhandurmaz/simpsonmaker/MainActivity.kt
+++ app/src/main/java/com/oguzhandurmaz/simpsonmaker/MainActivity.kt
@@ -6,16 +6,26 @@ import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private var name=""
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
 
     fun makeSimpson(view: View){
-        val name=nameText.text.toString()
-        val age=ageText.text.toString()
+        name=nameText.text.toString()
+        var age=ageText.text.toString().toIntOrNull()
+        if(age==null){
+            age=0
+        }
         val job=jobText.text.toString()
-        //val simpson=Simpson(name, age, job)
-        resultText.text="Name : ${name}, Age : ${age}, Job : ${job}"
+        val simpson=Simpson(name, age, job)
+        resultText.text="Name : ${simpson.name}, Age : ${simpson.age}, Job : ${simpson.job}"
+    }
+
+    fun exampleScope(view:View){
+        println(name)
     }
 }
--- app/src/main/java/com/oguzhandurmaz/simpsonmaker/Simpson.kt
+++ app/src/main/java/com/oguzhandurmaz/simpsonmaker/Simpson.kt
@@ -1,5 +1,11 @@
 package com.oguzhandurmaz.simpsonmaker
 
-class Simpson(var name:String,var age:Int,var job:String) {
+class Simpson(var name:String,var age:Int?,var job:String) {
+
+    var hairColor=""
+
+    fun changeHairColor(){
+        this.hairColor="Yellow"
+    }
 
 }
